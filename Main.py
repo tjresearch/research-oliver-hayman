88bb903ef32868936b94e8eb85c81564082aa04b
oh54321 <38576852+oh54321@users.noreply.github.com>
2020-05-05 00:27:04 -0400

    Update Main.py
    adding new code

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
 import PIL
+import time 
+import multiprocessing as mp
 import random
 import math
 from PIL import Image
-
+ROTNUM = 6
 from google.colab import drive
 from google.colab import files
 '''uploaded = files.upload()
@@ -26,7 +28,7 @@ def pix(s, img, n, theta, count):
   im = sav.load()
   a, b = sav.size
   wwww = color(im, n, a, b, count)
-  sav.save(s[:-4]+ "-"+str(theta)+".jpg")
+  #sav.save(s[:-4]+ "-"+str(theta)+".jpg")
   return wwww
 def color(im, n, width, height, count):
   arr = []
@@ -46,9 +48,9 @@ def label(im, a, b, c, arr, counttt):
       #im[a+i, b+j] = (255, 255, 255)
   if float(count)/float(c*c) < 0.6:
     arr[int(a/c)][int(b/c)] = counttt
-    for i in range(c):
+    '''for i in range(c):
       for j in range(c):
-        im[a+i,b+j] = (0, 0, 0)
+        im[a+i,b+j] = (0, 0, 0)'''
 def pixelate(s, temp, theta, width, side, count):
   return pix(s, temp, scaleConvert(temp, width, side), theta, count)
 def printer(x):
@@ -164,16 +166,16 @@ def widthFind(width, rot, rot2):
   a = abs(math.cos(rot+rot2)*width/math.sin(rot))
   b = abs(math.cos(rot-rot2)*width/math.sin(rot))
   return max(a, b)
-def pixArr(widths, side):
+def pixArr(widths, side, names):
   images = {}
   for i in range(len(widths)-1):
     images[i] = []
-    u = Image.open(str(i+1)+".png")
+    u = Image.open(names[i])
     widthh, heightt = u.size
-    for j in range(27):
-      v = u.rotate(j*360.0/27.0)
+    for j in range(ROTNUM):
+      v = u.rotate(j*360.0/ROTNUM)
       w, h = v.size
-      images[i].append(truncate(pixelate(str(i+1)+".png", u, j*360.0/27.0, w/widthh*widths[i], side, i+2)))
+      images[i].append(truncate(pixelate(names[i]+".png", u, j*360.0/ROTNUM, w/widthh*widths[i], side, i+2)))
   return images
 def size(x):
   count = 0
@@ -226,8 +228,8 @@ def imageCreator(arr, scale):
                   for l in range(scale*j, scale*(j+1)):
                       pixFin[k, l] = pixCol[temp]
   out.save("final.jpg")
-def pack(widths, side):
-  images = pixArr(widths, side)
+def pack(widths, side, names):
+  images = pixArr(widths, side, names)
   container = createContainer(widths, side)
   temp = []
   for i in range(len(widths)-1):
@@ -235,34 +237,52 @@ def pack(widths, side):
   temp.sort(reverse = True)
   bumperContainer = matBumperDown(container)
   bumperArr = {i:[matBumperUp(j) for j in images[i]] for i in range(len(widths)-1)}
-  print([size(images[0][i]) - size(bumperArr[0][i]) for i in range(len(images[0]))])
+  #print([size(images[0][i]) - size(bumperArr[0][i]) for i in range(len(images[0]))])
   #printer(container)
   #printer(bumperContainer)
   posRot = {}
+  maxh = 0
+  counthy = 0
   for aa in temp:
+    counthy += 1
+    print("Packing object", counthy)
     j, i = aa
     xxx, yyy, zzz = rotPlace(container, bumperContainer, images[i], bumperArr[i], side)
+    maxh = max(len(container) - yyy, len(container) - maxh)
     posRot[i] = xxx
     bumperContainer = matBumperDown(container)
+  print(maxh/len(container))
   return container
+def minWidthFind(x, y, z):
+   a, b = place(x, y)
+   return (a, b, z)
+def fileReader(fileName):
+  widths = []
+  names = []
+  '''for i in open(fileName, "r").readlines():
+    temp = i.replace("\n", "").strip().split(" ")
+    if len(temp) > 2:
+      for i in range(int(temp[2])-1):
+        widths.append(float(temp[1]))
+        names.append(temp[0])
+    widths.append(float(temp[1]))
+    names.append(temp[0])'''
+  nn = int(input("How many objects do you want to pack?"))
+  for i in range(nn):
+    names.append(input("Enter an object file name:"))
+    widths.append(float(input("Enter its width (in):")))
+  names.append(input("Enter a container file name:"))
+  widths.append(float(input("Enter its width (in):")))
+  return widths, names
 def rotPlace(container, bumperContainer, img, bumpImg, side):
-  minH, minW = place(bumperContainer, bumpImg[0])
-  current = 0
-  for j in range(3):
-    c1 = (current + 3**(2-j)) % 27
-    c2 = (current - 3**(2-j)) % 27
-    temp1, temp2 = place(bumperContainer, bumpImg[c1])
-    if temp1 < minW:
-      print(":)")
-      minH, minW = temp1, temp2
-      current = c1
-    else:
-      temp1, temp2 = place(bumperContainer, bumpImg[c2])
-      if temp1 < minW:
-          minH, minW = temp1, temp2
-          current = c2
-  placeHelp(container, img[current], minW, minH)
-  return (current, minH, minW)
+  #minH, minW = place(bumperContainer, bumpImg[0])
+  pool = mp.Pool(mp.cpu_count())
+  results = pool.starmap(minWidthFind, [(bumperContainer, bumpImg[i], i) for i in range(ROTNUM)])
+  print(results)
+  b, a, c = max(results)
+  pool.close()
+  placeHelp(container, img[c], a, b)
+  return (c, b, a)
 def canPlace(x, y, a, b):
   maxx = 0
   try:
@@ -295,12 +315,15 @@ def place(xx, yy):
       else:
         count -= temp - 1
   return (col, row)
-thetas = [0,0]
+time1 = time.time()
 s = [[0, 0, 0, 0, 1], [1, 1, 1, 1, 1]]
-widths = [2, 2, 2, 2, 2, 2, 2, 2,  2, 2,2, 2, 2, 2, 2, 2, 2, 2,  2, 2,2, 2, 2, 2, 2, 2, 2, 2,  2, 2,2, 2, 2, 2, 2, 2, 2, 2,  2, 2,2, 2, 2, 2, 2, 2, 2, 2,  2, 2,2, 2, 2, 2, 2, 2, 2, 2,  2, 2,2, 2, 2, 2, 2, 2, 2, 2,  2, 2,2, 2, 2, 2, 2, 2, 2, 2,  2, 2,2, 2, 2, 2, 2, 2, 2, 2,  2, 2,2, 2, 2, 2, 2, 2, 2, 2,  2, 2,10]
+#widths = [2, 2, 5]
+widths, names = fileReader("objects.txt")
   #test.append(list(1 for x in range(100)))'''
 #printer(matBumperUp(test))
 #printer(matBumperUp(s))
 #printer(matBumperUp(truncate(pixelate("1.png", 0, 2, 0.08, 1))))
-duplicate("images.png", 101)
-imageCreator(pack(widths, 0.04), 3)
+print("Number of processors: ", mp.cpu_count())
+imageCreator(pack(widths, 0.06, names), 3)
+print("Completed!")
+#print("Duration:", (time.time()-time1))
